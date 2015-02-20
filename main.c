3601a36d4e90ecdb8862064d4d6798eb0c3b4398
Diego De Leonardis <diegowazzaap@hotmail.it>
2015-02-20 08:49:13 +0100

    Commit

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+const int DIM = 5;
 int main(int argc, char** argv) {
-
+    int i, mag = 0;
+    int vettore[DIM];
+    printf("Inserisci l'eta' di %d persone\n", DIM);
+    for (i = 0; i < DIM; i++) {
+        printf("Inserisci l'eta' della %da persona : ", i + 1);
+        scanf("%d", &(vettore[i]));
+    }
+    for (i = 0; i < DIM; i++) {
+        if (vettore[i] > 17) {
+            mag++;
+        }
+    }
+    printf("I maggiorenni sono : %d", mag);
     return (EXIT_SUCCESS);
 }
 
